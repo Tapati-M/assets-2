d3b0f41f65130d11e5d61adb07f60dd9c1925e06
Tapati-M <tapatimondal6162@gmail.com>
2024-12-10 18:45:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,156 @@
+import logo1 from "./logo1.png"
+import bg from "./bg.png"
+import card from "./card.png"
+import card2 from "./card2.png"
+import card3 from "./card3.png"
+import card4 from "./card4.png"
+import manoj from "./manoj.png"
+import pramukh from "./pramukh.png"
+import prattush from "./prattush.png"
+import bhairava from "./bhairava.png"
+import rudrakh from "./rudrakh.png"
+import Horo from "./Horo.png"
+import kundli from "./kundli.png"
+import GoldCoin from "./GoldCoin.png"
+import RudrakshaBracelet from "./RudrakshaBracelet.png"
+import RudrakshaPendent from "./RudrakshaPendent.png"
+import PearlRing from "./PearlRing.png"
+import GreenJade from "./GreenJade.png"
+import pic1 from "./pic1.jpg"
+import pic2 from "./pic2.png"
+import pic3 from "./pic3.jpg"
+import Kamalesh from "./Kamalesh.png"
+import Design from "./Design.png"
+import Review1 from "./Review1.png"
+import Review2 from "./Review2.png"
+import Review3 from "./Review3.png"
+import Careerrun from "./Careerrun.png"
+import KundliPic from "./KundliPic.png"
+import Front from "./Front.png"
+import Problem from "./Problem.png"
+import Love from "./Love.png"
+import Finance from "./Finance.png"
+import Career from "./Career.png"
+import Child from "./Child.png"
+import Marriage from "./Marriage.png"
+import Education from "./Education.png"
+import Health from "./Health.png"
+import child1 from "./child1.png"
+import child2 from "./child2.png"
+import child3 from "./child3.png"
+import education1 from "./education1.png"
+import education2 from "./education2.png"
+import education3 from "./education3.png"
+import health1 from "./health1.jpg"
+import finance1 from "./finance1.png"
+import finance2 from "./finance2.png"
+import finance3 from "./finance3.png"
+import bgmarriage from "./bgmarriage.png"
+import Aries from "./Aries.png"
+import Taurus from "./Taurus.png"
+import Gemini from "./Gemini.png"
+import Cancer from "./Cancer.png"
+import Leo from "./Leo.png"
+import Virgo from "./Virgo.png"
+import Libra from "./Libra.png"
+import Scorpio from "./Scorpio.png"
+import Sagittarius from "./Sagittarius.png"
+import Capricorn from "./Capricorn.png"
+import Aquarius from "./Aquarius.png"
+import Pisces from "./Pisces.png"
+import Weekly from "./Weekly.png"
+import Monthly from "./Monthly.png"
+import Yearly from "./Yearly.png"
+import Car1 from './Car1.jpeg'
+import Car2 from './Car2.jpeg'
+import SP1 from './SP1.jpg'
+import SP2 from './SP2.jpg'
+import RM1 from './RM1.jpg'
+import RM2 from './RM2.jpg'
+import Laughingbuddha1 from './Laughingbuddha1.jpg'
+import Laughingbuddha2 from './Laughingbuddha2.jpg'
+import Emerald1 from './Emerald1.jpg'
+import Emerald2 from './Emerald2.jpg'
+import BlueSapphires1 from './BlueSapphires1.jpg'
+import BlueSapphires2 from './BlueSapphires2.jpg'
+import Sapphires1 from './Sapphires1.jpg'
+import Sapphires2 from './Sapphires2.jpg'
+export {
+    logo1,
+    bg,
+    card,
+    card2,
+    card3,
+    card4,
+    manoj,
+    pramukh,
+    prattush,
+    rudrakh,
+    bhairava,
+    Horo,
+    kundli,
+    GreenJade,
+    PearlRing,
+    GoldCoin,
+    RudrakshaBracelet,
+    RudrakshaPendent,
+    pic1,
+    pic2,
+    pic3,
+    Kamalesh,
+    Design,
+    Review1,
+    Review2,
+    Review3,
+    Careerrun,
+    KundliPic,
+    Front,
+    Problem,
+    Love,
+    Career,
+    Marriage,
+    Education,
+    Child, 
+    Finance,
+    Health,
+    child1,
+    child2,
+    child3,
+    education1,
+    education2,
+    education3,
+    health1,
+    finance1,
+    finance2,
+    finance3,
+    bgmarriage,
+    Aries,
+    Taurus,
+    Libra,
+    Sagittarius,
+    Aquarius,
+    Scorpio,
+    Gemini,
+    Cancer,
+    Capricorn,
+    Leo,
+    Virgo,
+    Pisces,
+    Weekly,
+    Monthly,
+    Yearly,
+    Car1,
+    Car2,
+    SP1,
+    SP2,
+    Sapphires1,
+    Sapphires2,
+    RM1,
+    RM2,
+    BlueSapphires1,
+    BlueSapphires2,
+    Emerald1,
+    Emerald2,
+    Laughingbuddha1,
+    Laughingbuddha2,
+};
